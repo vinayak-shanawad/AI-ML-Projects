1c977cb2bc08ab63e1177a63b5cfb1c521eefad4
Vinayak.Shanawad <Vinayak.Shanawad@accrete.ai>
2023-02-15 08:46:51 +0530

    added sagemaker shadow deployment changes

--- 06_SageMakerShadowDeployment/install-codeserver-tensorboard.sh
+++ 06_SageMakerShadowDeployment/install-codeserver-tensorboard.sh
@@ -8,7 +8,7 @@ set -eux
 #  VARIABLES  #
 ###############
 
-CODE_SERVER_VERSION="4.9.1"
+CODE_SERVER_VERSION="v4.10.0-rc.2"
 CODE_SERVER_INSTALL_LOC="/opt/.cs"
 XDG_DATA_HOME="/opt/.xdg/data"
 XDG_CONFIG_HOME="/opt/.xdg/config"
@@ -136,7 +136,7 @@ then
 	pip install importlib_metadata
 	
 	# Configure TensorBoard to run automatically
-	echo "tensorboard --logdir=\$SM_MODEL_DIR --host=0.0.0.0 --port=6006 &" >> ~/.bashrc
+	# echo "tensorboard --logdir=\$SM_MODEL_DIR --host=0.0.0.0 --port=6006 &" >> ~/.bashrc
 	
 	conda deactivate
 
